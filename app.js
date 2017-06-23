00e8fe3ab97fbf56d28e1a272950528aade9f574
Dakshesh Patel <dakspate@in.ibm.com>
2017-06-23 11:54:50 +0530

    refactor code

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2015 IBM Corp. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
 var express = require('express'); // app server
@@ -25,14 +9,8 @@ var app = express();
 // Bootstrap application settings
 app.use(express.static('./public')); // load UI from public folder
 app.use(bodyParser.json());
-//var workspace = '2aca8b40-c85d-43fd-a013-c4cbc0142b4b';
 // Create the service wrapper
 var conversation = new Conversation({
-  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
-  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-   //username: 'f73ea4ad-8621-4ae1-87eb-3cab886f46b5',
-   //password: 'soELvOZZKhEd',
-  //url: 'https://gateway.watsonplatform.net/conversation/api',
   version_date: Conversation.VERSION_DATE_2017_04_21
 });
 
@@ -81,25 +59,7 @@ function updateMessage(resHTTP,input, response) {
      }else{
        return resHTTP.json( response);
      }
-    //return response;
   }
-/*  if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
-    } else {
-      responseText = 'I did not understand your intent';
-    }
-  }
-  response.output.text = responseText;*/
-  //return response;
 }
 
 /**
@@ -109,13 +69,6 @@ function updateMessage(resHTTP,input, response) {
 */
 function getCalculationResult(resHTTP,response){ //An array holding the operands
 var numbersArr = [];
-//Fill the content of the array with the entities of type 'sys-number'
-/*for (var i = 0; i < response.entities.length; i++) {
-    if (response.entities[i].entity === 'sys-number')
-    {
-        numbersArr.push(response.entities[i].value);
-      }
-}*/
 // In case the user intent is add, perform the addition
 // In case the intent is multiply, perform the multiplication
    var result = 0;
